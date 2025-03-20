9e7fc93468de76dda987feae3e6cfb6d47134560
CarvalhoNathan <fariasnathan75@gmail.com>
2025-03-19 23:18:36 -0300

    Projeto finalizado

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+
+string cebolinha;
+
+Console.WriteLine("Escreva uma frase: ");
+cebolinha = Console.ReadLine()!;
+
+Console.WriteLine($"Frase do cebolinha: {cebolinha.Replace('r', 'l')}");
